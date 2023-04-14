3f7db8ff8713a299da10b2301daeee319beaf076
unknown <gippatrick@gmail.com>
2023-04-14 15:07:18 -0700

    Updated Navbar component home button

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,29 +1,48 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
-import Logo from '@/assets/Logo.png'
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
 
-type Props = {}
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
 
   return (
     <nav>
-      <div
-        className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
-      > 
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
-          <div className={`${flexBetween} w-full gap-16`} >
+          <div className={`${flexBetween} w-full gap-16`}>
             {/* LEFT SIDE */}
             <img alt="logo" src={Logo} />
 
             {/* RIGHT SIDE */}
             <div className={`${flexBetween} w-full `}>
               <div className={`${flexBetween} gap-8 text-sm`}>
-                <p>Home</p>
-                <p>Benefits</p>
-                <p>Our Classes</p>
-                <p>Contact Us</p>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Out Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
               </div>
               <div className={`${flexBetween} gap-8`}>
                 <p>Sign In</p>
@@ -34,7 +53,7 @@ const Navbar = (props: Props) => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
