ffb5faf584716927e6d15239a068078e873660a9
unknown <gippatrick@gmail.com>
2023-04-16 22:23:49 -0700

    Added ContactUs Page

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,14 @@
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  return <div>index</div>;
+};
+
+export default ContactUs;
